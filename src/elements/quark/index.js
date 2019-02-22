d7613069b1cc58c4c652375d9a5f7e53a52d71bc
Eddort <webisforce@gmail.com>
2019-02-22 13:35:44 +0300

    Подключил контекст

---
+++ src/elements/quark/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const QuarkContext = React.createContext();
+
+export const Provider = QuarkContext.Provider;
+
+export function asQuark(Component) {
+    return function ConnectedComponent(props) {
+      return (
+        <QuarkContext.Consumer>
+          {qState => <Component {...props} qState={qState}/>}
+        </QuarkContext.Consumer>
+      );
+    }
+  }
