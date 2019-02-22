c61103b52e3684dca0f29392ac87e3234dd12214
Eddort <webisforce@gmail.com>
2019-02-22 22:01:36 +0300

    стори текста

--- src/text/text.stories.js
+++ src/text/text.stories.js
@@ -4,23 +4,16 @@ import { withKnobs, text } from '@storybook/addon-knobs';
 import Text from './index';
 import Theme from '../theme';
 
-const Button = () => <button>Hello</button>;
 const stories = storiesOf('Text', module);
+
 stories.addDecorator(withKnobs);
+
 stories
   .add('default', () => (
     <Theme>
-      <Button />
-      <Text p={[1, 200, 55]} display={['block', 'none', 'block']} variant="primary" />
-      <Button />
-    </Theme>
-  ))
-  .add('100px', () => (
-    <Theme>
-      <Button />
-      <Text height={text('height', '100px')}> привет</Text>
-      sss
-      <Text height={[100, 20, 30]} />
-      <Button />
+      <Text>Lorem ....</Text>
+      <Text>Lorem ....</Text>
+      <Text>Lorem ....</Text>
+      <Text>Lorem ....</Text>
     </Theme>
   ));
