10b369b725a1cc73c656f9f0073fde224bb80c1d
Eddort <webisforce@gmail.com>
2019-02-27 17:19:42 +0300

    flex

--- src/flex/index.js
+++ src/flex/index.js
@@ -1,20 +1,5 @@
-import React from 'react';
-import injectSheet from 'react-jss';
-import { includeWith, themed, variant, className } from '../styled';
+import styled from 'styled-components';
+import { includeWith } from '../styled';
+import Box from '../box';
 
-const styles = theme => ({
-  flex(props) {
-    const css = {
-      display: 'flex',
-      ...themed('Flex')(theme),
-      ...variant('flexs')({ theme, ...props }),
-      ...includeWith('defaults', props),
-      ...includeWith('flex', props),
-    };
-    return css;
-  },
-});
-
-const Card = props => <div className={className('flex', props)}>{props.children}</div>;
-
-export default injectSheet(styles)(Card);
+export default styled(Box)(...includeWith('flex'));
