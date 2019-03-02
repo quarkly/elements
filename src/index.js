90db497c6879ba180f60a3e65f4a726ccde26a39
Eddort <webisforce@gmail.com>
2019-03-02 11:15:51 +0300

    DI for themes

--- src/index.js
+++ src/index.js
@@ -6,3 +6,4 @@ export { default as Carousel } from './carousel';
 export { default as Button } from './button';
 export { default as Text } from './text';
 export { default as Flex } from './flex';
+export { default as Quark } from './quark';
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from './default';
+import { asQuark } from '../quark';
 
-export default props => (
-  <ThemeProvider theme={props.theme || theme}>
-    <React.Fragment>{props.children}</React.Fragment>
-  </ThemeProvider>
-);
+const qStateDefault = {
+  themes: {},
+};
+
+export default asQuark(props => {
+  const { name } = props;
+  const qState = props.qState || qStateDefault;
+  let diTheme;
+  if (name && qState.themes[name]) {
+    diTheme = qState.themes[name];
+  }
+  return (
+    <ThemeProvider theme={props.theme || diTheme || theme}>
+      <React.Fragment>{props.children}</React.Fragment>
+    </ThemeProvider>
+  );
+});
