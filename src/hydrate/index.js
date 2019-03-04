7efddcc156c0cb42bf917759af20d993c09d4d81
Eddort <webisforce@gmail.com>
2019-03-04 23:46:50 +0300

    Публикация с темами

--- src/hydrate/index.js
+++ src/hydrate/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as elements from '../index';
 
+const { Theme, Provider } = elements;
+
 Object.assign(window, { React, ...elements });
-const app = window.qApp;
-ReactDOM.hydrate(app(), document.getElementById('root'));
+
+const { qConf, qApp } = window;
+
+ReactDOM.hydrate(
+  <Provider value={qConf}>
+    <Theme name="quarkly">{qApp({})}</Theme>
+  </Provider>,
+  document.getElementById('root'),
+);
