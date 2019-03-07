46a9c92dc8a295b171d8f3a4d5a8fb0808973aa5
Eddort <webisforce@gmail.com>
2019-03-07 12:07:18 +0300

    добавил новые элементы

--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ export { default as Carousel } from './carousel';
 export { default as Button } from './button';
 export { default as Text } from './text';
 export { default as Flex } from './flex';
+export { default as Hr } from './hr';
+export { default as Grid } from './grid';
+export { default as Box } from './box';
+export { default as Block } from './block';
 export * from './quark';
