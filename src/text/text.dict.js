1b96aa79fbf864857adc6cd0bc0b86719c307006
Artem <psycholcycle@gmail.com>
2019-03-21 08:57:05 +0000

    text dict

---
+++ src/text/text.dict.js
@@ -0,0 +1,13 @@
+export default {
+  props: {
+    styles: ['defaults', 'text'],
+    actions: [],
+    attributes: {
+      variant: {
+        handler: 'text',
+        type: String,
+      },
+    },
+  },
+  description: 'Элемент text',
+};
