04113a08e71226825ae4c1a13f26cb7e6e71d6fd
Artem <psycholcycle@gmail.com>
2019-03-21 08:40:45 +0000

    block dict

---
+++ src/block/block.dict.js
@@ -0,0 +1,13 @@
+export default {
+  props: {
+    styles: ['box'],
+    actions: [],
+    attributes: {
+      variant: {
+        handler: 'text',
+        type: String,
+      },
+    },
+  },
+  description: 'Элемент Block',
+};
