d34b6541cd67a662efc28fd704c7d4837cc2ef1b
Artem <psycholcycle@gmail.com>
2019-03-21 08:51:40 +0000

    hr dict

---
+++ src/hr/button.dict.js
@@ -0,0 +1,13 @@
+export default {
+  props: {
+    styles: ['hr'],
+    actions: [],
+    attributes: {
+      variant: {
+        handler: 'text',
+        type: String,
+      },
+    },
+  },
+  description: 'Элемент hr',
+};
