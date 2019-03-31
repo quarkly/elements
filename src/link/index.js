f9e1609d6de0deebcf607e8079d67ebfb4d77b66
Eddort <webisforce@gmail.com>
2019-03-31 12:49:55 +0300

    Фикс багов

--- src/link/index.js
+++ src/link/index.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import { includeWith, themed, variant } from '../styled';
 
 export default styled('a')(
-  {
-  },
+  {},
   themed('Link'),
   variant('links'),
-  ...includeWith('default, text, hovered'),
+  ...includeWith('defaults'),
+  ...includeWith('text'),
 );
