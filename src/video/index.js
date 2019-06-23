f3c36b2226b65c4976ed04fd9bb8436c2f2d122c
Artem <psycholcycle@gmail.com>
2019-06-23 20:22:47 +0300

    wip video added extra options

--- src/video/index.js
+++ src/video/index.js
@@ -9,39 +9,101 @@ const VideoWrap = elementary.div({
   },
 });
 
-const NewVideo = ({ src, mute, showControls, showInfo, loop, autoplay, ...other }) => {
+const NewVideo = ({ src, mute, showControls, showInfo, loop, autoPlay, ...other }) => {
   const parseUrl = new URL(src);
   const videoHost = parseUrl.hostname;
   let TypeOfVideo;
   let videoId;
+  let muteOption;
+  let autoPlayOption;
+  let showControlsOption;
+  let loopOption;
+  let showInfoOption;
+  let extraOptions;
+
   // YOUTUBE autoplay - добавляем к URL-строке &autoplay=1 (0* или 1)
   // YOUTUBE showControls - добавляем к URL-строке &controls=1 (0 или 1* или 2)
   // YOUTUBE loop - добавляем к URL-строке &loop=1 (0* или 1)
   // YOUTUBE showInfo - добавляем к URL-строке &showinfo=1 (0 или 1*)
   // YOUTUBE mute - добавляем к URL-строке &mute=1 (0* или 1)
+
+  // VIMEO ссылки на видео могут быть разные https://developer.vimeo.com/api/oembed/videos
+  // VIMEO autoplay - добавляем к URL-строке &autoplay=1 (0* или 1)
+  // VIMEO showControls - добавляем к URL-строке &controls=1 (0 или 1* или 2)
+  // VIMEO loop - добавляем к URL-строке &loop=1 (0* или 1)
+  // VIMEO showInfo - добавляем &title=0 &byline=0 (0 или 1*)
+  // VIMEO mute - добавляем к URL-строке &muted=1 (0* или 1)
+
   if (videoHost === 'www.youtube.com' || videoHost === 'youtube.com' || videoHost === 'youtu.be') {
     videoId = parseUrl.searchParams.get('v');
     TypeOfVideo = 'youtube';
   } else if (videoHost === 'vimeo.com') {
     videoId = parseUrl.pathname;
-    TypeOfVideo = 'vimeoVideoId';
+    TypeOfVideo = 'vimeo';
   } else {
     TypeOfVideo = 'unknown';
   }
 
+  // autoplay
+  if (autoPlay) {
+    autoPlayOption = '&autoplay=1';
+  } else {
+    autoPlayOption = '&autoplay=0';
+  }
+
+  // showControls
+  if (showControls) {
+    showControlsOption = '&controls=1';
+  } else {
+    showControlsOption = '&controls=0';
+  }
+
+  // loopOption
+  if (loop) {
+    loopOption = '&loop=1';
+  } else {
+    loopOption = '&loop=0';
+  }
+
+  // showInfo youtube
+  if (showInfo && TypeOfVideo === 'youtube') {
+    showInfoOption = '&showinfo=1';
+  } else {
+    showInfoOption = '';
+  }
+
+  // showInfo vimeo
+  if (showInfo && TypeOfVideo === 'vimeo') {
+    showInfoOption = '&title=0&byline=0';
+  } else {
+    showInfoOption = '';
+  }
+
+  // MUTE
+  if (mute && TypeOfVideo === 'youtube') {
+    muteOption = '&mute=1';
+  } else if (mute && TypeOfVideo === 'vimeo') {
+    muteOption = '&muted=1';
+  } else if (mute === false && TypeOfVideo === 'vimeo') {
+    muteOption = '&muted=0';
+  } else {
+    muteOption = '&mute=0';
+  }
+  extraOptions = muteOption + autoPlayOption + showControlsOption + loopOption + showInfoOption;
+
   return (
     <VideoWrap {...other}>
-      <Video videoId={videoId} TypeOfVideo={TypeOfVideo} />
+      <Video videoId={videoId} TypeOfVideo={TypeOfVideo} extraOptions={extraOptions} />
     </VideoWrap>
   );
 };
 
-const Video = ({ videoId, TypeOfVideo }) => {
+const Video = ({ videoId, extraOptions, TypeOfVideo }) => {
   let embedVideo;
 
-  const defaultVideo = 'https://www.youtube.com/embed/eE8Awccr-Ew';
-  const youtubeVideo = `https://www.youtube.com/embed/${videoId}?rel=0`;
-  const vimeoVideo = `https://player.vimeo.com/video/${videoId}?byline=0&portrait=0`;
+  const defaultVideo = `https://www.youtube.com/embed/eE8Awccr-Ew?rel=0${extraOptions}`;
+  const youtubeVideo = `https://www.youtube.com/embed/${videoId}?rel=0${extraOptions}`;
+  const vimeoVideo = `https://player.vimeo.com/video${videoId}?portrait=0&responsive=1${extraOptions}`;
 
   if (TypeOfVideo === 'youtube') {
     embedVideo = youtubeVideo;
@@ -54,10 +116,11 @@ const Video = ({ videoId, TypeOfVideo }) => {
     <iframe
       title="video"
       width="100%"
-      height="auto"
+      height="100%"
       src={embedVideo}
       frameBorder="0"
       allowFullScreen
+      allow="autoplay; fullscreen"
     />
   );
 };
--- src/video/video.stories.js
+++ src/video/video.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, number, text, select, color, boolean } from '@storybook/addon-knobs';
 import Video from './index';
 import Theme from '../theme';
 
@@ -9,5 +9,269 @@ const stories = storiesOf('Video', module);
 stories.addDecorator(withKnobs);
 
 stories.add('default', () => (
-  <Video src="https://www.youtube.com/watch?v=c6t3bW7kx6E" width="100%" height="auto" />
+  <Theme>
+    <Video
+      src={text('src', 'https://www.youtube.com/watch?v=c6t3bW7kx6E', 'Core')}
+      mute={boolean('mute', '0', 'Core')}
+      showControls={boolean('showControls', '1', 'Core')}
+      showInfo={boolean('showInfo', '1', 'Core')}
+      loop={boolean('loop', '0', 'Core')}
+      autoPlay={boolean('autoPlay', '0', 'Core')}
+      p={text('p', '', 'Spacing')}
+      paddingTop={text('paddingTop', '', 'Spacing')}
+      paddingRight={text('paddingRight', '', 'Spacing')}
+      paddingBottom={text('paddingBottom', '', 'Spacing')}
+      paddingLeft={text('paddingLeft', '', 'Spacing')}
+      px={text('px', '', 'Spacing')}
+      py={text('py', '', 'Spacing')}
+      m={text('m', '', 'Spacing')}
+      marginTop={text('marginTop', '', 'Spacing')}
+      marginRight={text('marginRight', '', 'Spacing')}
+      marginBottom={text('marginBottom', '', 'Spacing')}
+      marginLeft={text('marginLeft', '', 'Spacing')}
+      mx={text('mx', '', 'Spacing')}
+      my={text('my', '', 'Spacing')}
+      bg={color('bg', '', 'Background')}
+      display={select(
+        'display',
+        {
+          block: 'block',
+          flex: 'flex',
+          inline: 'inline',
+          'inline-block': 'inline-block',
+          grid: 'grid',
+          none: 'none',
+        },
+        'block',
+        'Layout',
+      )}
+      size={text('size', '', 'Layout')}
+      width={text('width', '100%', 'Layout')}
+      minWidth={text('minWidth', '', 'Layout')}
+      maxWidth={text('maxWidth', '', 'Layout')}
+      height={text('height', '100vh', 'Layout')}
+      minHeight={text('minHeight', '', 'Layout')}
+      maxHeight={text('maxHeight', '', 'Layout')}
+      alignItems={select(
+        'align-items',
+        {
+          'flex-start': 'flex-start',
+          'flex-end': 'flex-end',
+          center: 'center',
+          baseline: 'baseline',
+          stretch: 'stretch',
+        },
+        'stretch',
+        'Flexbox',
+      )}
+      justifyContent={select(
+        'justifyContent',
+        {
+          'flex-start': 'flex-start',
+          'flex-end': 'flex-end',
+          center: 'center',
+          'space-between': 'space-between',
+          'space-around': 'space-around',
+          'space-evenly': 'space-evenly',
+        },
+        'flex-start',
+        'Flexbox',
+      )}
+      flexWrap={select(
+        'flexWrap',
+        {
+          nowrap: 'nowrap',
+          wrap: 'wrap',
+          'wrap-reverse': 'wrap-reverse',
+        },
+        'nowrap',
+        'Flexbox',
+      )}
+      flexDirection={select(
+        'flexDirection',
+        {
+          row: 'row',
+          'row-reverse': 'row-reverse',
+          column: 'column',
+          'column-reverse': 'column-reverse',
+        },
+        'row',
+        'Flexbox',
+      )}
+      flex={text('flex', '', 'Flexbox')}
+      alignContent={select(
+        'alignContent',
+        {
+          'flex-start': 'flex-start',
+          'flex-end': 'flex-end',
+          center: 'center',
+          'space-between': 'space-between',
+          'space-around': 'space-around',
+          'space-evenly': 'space-evenly',
+          stretch: 'stretch',
+        },
+        'flex-start',
+        'Flexbox',
+      )}
+      justifyItems={select(
+        'justifyItems',
+        {
+          stretch: 'stretch',
+          center: 'center',
+          start: 'start',
+          end: 'end',
+          baseline: 'baseline',
+          inherit: 'inherit',
+        },
+        'stretch',
+        'Flexbox',
+      )}
+      justifySelf={select(
+        'justifySelf',
+        {
+          'flex-start': 'flex-start',
+          'flex-end': 'flex-end',
+          center: 'center',
+          'space-between': 'space-between',
+          'space-around': 'space-around',
+          'space-evenly': 'space-evenly',
+        },
+        'flex-start',
+        'Flexbox',
+      )}
+      alignSelf={select(
+        'alignSelf',
+        {
+          auto: 'auto',
+          'flex-start': 'flex-start',
+          'flex-end': 'flex-end',
+          center: 'center',
+          baseline: 'baseline',
+          stretch: 'stretch',
+        },
+        'flex-start',
+        'Flexbox',
+      )}
+      order={number('order', '', '{0}', 'Flexbox')}
+      flexBasis={text('flexBasis', '', 'Flexbox')}
+      gridGap={text('gridGap', '', 'Grid')}
+      gridRowGap={text('gridRowGap', '', 'Grid')}
+      gridColumnGap={text('gridColumnGap', '', 'Grid')}
+      gridColumn={text('gridColumn', '', 'Grid')}
+      gridRow={text('gridRow', '', 'Grid')}
+      gridArea={text('gridArea', '', 'Grid')}
+      gridAutoFlow={select(
+        'gridAutoFlow',
+        {
+          row: 'row',
+          column: 'column',
+          dense: 'dense',
+          'row dense': 'row dense',
+          'column dense': 'column dense',
+        },
+        'row',
+        'Grid',
+      )}
+      gridAutoRows={text('gridAutoRows', '', 'Grid')}
+      gridAutoColumns={text('gridAutoColumns', '', 'Grid')}
+      gridTemplateRows={text('gridTemplateRows', '', 'Grid')}
+      gridTemplateColumns={text('gridTemplateColumns', '', 'Grid')}
+      gridTemplateAreas={text('gridTemplateAreas', '', 'Grid')}
+      background={text('background', '', 'Background')}
+      backgroundImage={text('backgroundImage', 'url()', 'Background')}
+      backgroundSize={select(
+        'backgroundSize',
+        {
+          auto: 'auto',
+          cover: 'cover',
+          contain: 'contain',
+        },
+        'auto',
+        'Background',
+      )}
+      backgroundPosition={select(
+        'backgroundPosition',
+        {
+          'top left': 'top left',
+          'top center': 'top center',
+          'top right': 'top right',
+          'center left': 'center left',
+          'center center': 'center center',
+          'center right': 'center right',
+          'bottom left': 'bottom left',
+          'bottom center': 'bottom center',
+          'bottom right': 'bottom right',
+          inherit: 'inherit',
+        },
+        'center center',
+        'Background',
+      )}
+      backgroundRepeat={select(
+        'backgroundRepeat',
+        {
+          'no-repeat': 'no-repeat',
+          repeat: 'repeat',
+          'repeat-x': 'repeat-x',
+          'repeat-y': 'repeat-y',
+          space: 'space',
+          round: 'round',
+          inherit: 'inherit',
+        },
+        'no-repeat',
+        'Background',
+      )}
+      border={text('border', '', 'Misc')}
+      borderTop={text('borderTop', '', 'Misc')}
+      borderRight={text('borderRight', '', 'Misc')}
+      borderBottom={text('borderBottom', '', 'Misc')}
+      borderLeft={text('borderLeft', '', 'Misc')}
+      borderWidth={text('borderWidth', '', 'Misc')}
+      borderStyle={select(
+        'borderStyle',
+        {
+          none: 'none',
+          solid: 'solid',
+          hidden: 'hidden',
+          dashed: 'dashed',
+          dotted: 'dotted',
+          double: 'double',
+        },
+        'solid',
+        'Misc',
+      )}
+      borderColor={color('borderColor', '', 'Misc')}
+      borderRadius={text('borderRadius', '', 'Misc')}
+      boxShadow={text('boxShadow', '', 'Misc')}
+      opacity={number('opacity', '', '{1}', 'Misc')}
+      overflow={select(
+        'overflow',
+        {
+          visible: 'visible',
+          hidden: 'hidden',
+          scroll: 'scroll',
+          auto: 'auto',
+          inherit: 'inherit',
+        },
+        'visible',
+        'Misc',
+      )}
+      position={select(
+        'position',
+        {
+          absolute: 'absolute',
+          fixed: 'fixed',
+          relative: 'relative',
+          static: 'static',
+          inherit: 'inherit',
+        },
+        'static',
+        'Position',
+      )}
+      zIndex={number('zIndex', 1, 'Position')}
+      top={text('top', '', 'Position')}
+      right={text('right', '', 'Position')}
+      bottom={text('bottom', '', 'Position')}
+      left={text('left', '', 'Position')}
+    />
+  </Theme>
 ));
