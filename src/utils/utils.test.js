fc3a20e53097939f96173a02992039308b2d3241
Eddort <webisforce@gmail.com>
2019-06-30 15:23:08 +0300

    update snap

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -16,16 +16,24 @@ describe('getOmitProps', () => {
   test('defaults', () => {
     expect(getOmitProps()).toStrictEqual({
       qstate: true,
+      quarkinfo: true,
+      slottype: true,
     });
     expect(getOmitProps({})).toStrictEqual({
       qstate: true,
+      quarkinfo: true,
+      slottype: true,
     });
     expect(getOmitProps({ some: 123 })).toStrictEqual({
       qstate: true,
+      quarkinfo: true,
+      slottype: true,
       some: 123,
     });
     expect(getOmitProps({ some: 123, qstate: false })).toStrictEqual({
       qstate: false,
+      quarkinfo: true,
+      slottype: true,
       some: 123,
     });
   });
