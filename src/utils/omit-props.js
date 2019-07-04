ea2fee696d72dbf60e5f68b9b640ca3c4c5412b4
Eddort <webisforce@gmail.com>
2019-07-04 11:19:02 +0300

    omit props theme

--- src/utils/omit-props.js
+++ src/utils/omit-props.js
@@ -2,6 +2,7 @@ export const omitPropsDefault = {
   qstate: true,
   quarkinfo: true,
   slottype: true,
+  theme: true,
 };
 
 export default props => ({ ...omitPropsDefault, ...props });
--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -6,22 +6,26 @@ describe('getOmitProps', () => {
       qstate: true,
       quarkinfo: true,
       slottype: true,
+      theme: true,
     });
     expect(getOmitProps({})).toStrictEqual({
       qstate: true,
       quarkinfo: true,
       slottype: true,
+      theme: true,
     });
     expect(getOmitProps({ some: 123 })).toStrictEqual({
       qstate: true,
       quarkinfo: true,
       slottype: true,
+      theme: true,
       some: 123,
     });
     expect(getOmitProps({ some: 123, qstate: false })).toStrictEqual({
       qstate: false,
       quarkinfo: true,
       slottype: true,
+      theme: true,
       some: 123,
     });
   });
